8dbfd5ccc6cab8a261ffa39742e825268721d9a8
KevC000 <chenkevin1119@gmail.com>
2024-03-13 12:31:36 -0400

    refactored search bar to its own composable

--- app/src/main/java/com/yonasoft/jadedictionary/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/yonasoft/jadedictionary/ui/screens/search/SearchScreen.kt
@@ -1,65 +1,41 @@
 package com.yonasoft.jadedictionary.ui.screens.search
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.yonasoft.jadedictionary.ui.components.historyrow.HistoryRow
+import com.yonasoft.jadedictionary.ui.components.searchbar.JadeSearchBar
 
 
-@SuppressLint("StateFlowValueCalledInComposition")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(viewModel: SearchScreenViewModel = hiltViewModel()) {
 
-    val searchResults = viewModel.searchResults.collectAsState()
+    var query = viewModel.searchQuery.value
+    var active = viewModel.active.value
     val history = viewModel.history.collectAsState()
+
+    val searchResults = viewModel.searchResults.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        SearchBar(modifier = Modifier.fillMaxWidth(),
-            query = viewModel.searchQuery.value,
-            onQueryChange = { viewModel.searchQuery.value = it },
+        JadeSearchBar(
+            query = query,
+            active = active,
             onSearch = {
-                viewModel.addToHistory(it)
-                viewModel.active.value = false
-                viewModel.onSearch(it)
+                viewModel.onSearch()
             },
-            active = viewModel.active.value,
-            onActiveChange = {
-                viewModel.active.value = it
+            changeQuery = {
+                query = it
             },
-            placeholder = {
-                Text(text = "Search by English, Hanzi or Pinyin...")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
-            },
-            trailingIcon = {
-                Icon(
-                    modifier = Modifier.clickable {
-                        if (viewModel.searchQuery.value.isEmpty()) {
-                            viewModel.active.value = false
-                        }
-                        viewModel.searchQuery.value = ""
-                    }, imageVector = Icons.Default.Close, contentDescription = "Close Icon"
-                )
-
-            }) {
+            changeActive = { active = it }
+        ) {
             history.value.forEachIndexed { index, it ->
                 HistoryRow(
                     index = index, text = it,
@@ -79,7 +55,7 @@ fun SearchScreen(viewModel: SearchScreenViewModel = hiltViewModel()) {
             items(
                 searchResults.value.size
             ) {
-                val word = viewModel.searchResults.value[it]
+                val word = searchResults.value[it]
                 Text(text = word.simplified.toString())
             }
         }
